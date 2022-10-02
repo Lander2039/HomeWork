daf9172741cebcad7334a1f9924751e5fdb3ae18
Lander2039 <lander2039@gmail.com>
2022-10-02 15:36:13 +0300

    Add task 2 HomeWork5

--- HomeWork/src/main/java/com/example/homework/TestConflict.java
+++ HomeWork/src/main/java/com/example/homework/TestConflict.java
@@ -2,6 +2,6 @@ package com.example.homework;
 
 public class TestConflict {
     public static void main(String[] args){
-    System.out.println("Hello");
+    System.out.println("Hello000000");
 }
 }
